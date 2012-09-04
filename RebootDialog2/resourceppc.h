4492ba0b11fb0bcc25717da3e5c6ed4a0a93bcbe
hjgode@gmail.com <hjgode@gmail.com@55837afe-e278-a47e-9e9b-7c197034ff08>
2012-09-04 08:17:46 +0000

    added RebootDialog test

---
+++ RebootDialog2/resourceppc.h
@@ -0,0 +1,38 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by RebootDialog2sp.rc
+//
+#define IDS_APP_TITLE                   1
+#define IDC_REBOOTDIALOG                2
+#define IDS_REBOOTDIALOG2               2
+#define IDR_MENU                        102
+#define IDS_QUIT                        104
+#define IDS_BTNWARMBOOT                 105
+#define IDS_BTNSHUTDOWN                 106
+#define IDS_BTNSUSPEND                  107
+#define IDS_BTNCANCEL                   108
+#define IDS_DIALOGTEXT                  109
+#define IDS_BTNCLEANBOOT                110
+#define IDD_REBOOTDIALOG                132
+#define IDC_PROGRESS1                   1006
+#define IDC_EDIT2                       1008
+#define IDC_BTNWARMBOOT                 1010
+#define IDC_BTNSHUTDOWN                 1011
+#define IDC_BTNCANCEL                   1012
+#define IDC_BTNSUSPEND                  1013
+#define IDC_DIALOGTEXT                  1014
+#define IDC_BTNCLEANBOOT                1015
+#define IDM_QUIT                        40001
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
